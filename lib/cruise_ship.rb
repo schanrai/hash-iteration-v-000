12feb51b244b06ef249f1fe846987c0ec01cf5ec
Sushi Chanrai <sushi@pathworks.co>
2018-07-05 23:43:59 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,7 +9,7 @@ passengers = {
 def select_winner(passengers)
   # add the code snippet here!
   winner = ""
-  passengers.each do |suite, name|
+  passengers.each do|suite, name|
     if suite == :suite_a && name.start_with?("A")
       winner = name
   end
